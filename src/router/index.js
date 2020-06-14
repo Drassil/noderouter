52dece1092e8c497b1bd6fb135977bcf3c7dae26
Yehonal <yehonal.azeroth@gmail.com>
2020-06-14 22:18:08 +0200

    feat: all-in-one router and client

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 const ApiServer = require('./apiServer');
+const fs = require('fs');
+const path = require('path');
 
 process.on('uncaughtException', function(error) {
   console.error('Uncaught Exception: ', error);
@@ -10,4 +12,16 @@ process.on('SIGINT', function() {
   process.exit();
 });
 
+// start API server
 new ApiServer({});
+
+// if filename specified, then run client too
+const arg = process.argv[2];
+
+const file = arg ? arg : process.env.NR_HOSTS_FILE;
+
+if (file && fs.existsSync(file)) {
+  const {ClientMgr} = require('../client/index');
+
+  ClientMgr.registerHostsWithFile(path.resolve(file));
+}
